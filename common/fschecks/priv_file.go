ea190381e4dd348040a9f6ba0723c54d4849c16e
RomanChebotarev <RomanCh@artel.im>
2022-10-16 03:46:06 +0300

    testing - add common/fschecks

--- common/fschecks/priv_file.go
+++ common/fschecks/priv_file.go
@@ -7,6 +7,16 @@ import (
 	"syscall"
 )
 
+type ErrOwnership struct {
+	err error
+}
+func (e *ErrOwnership) Error() string {
+	return e.err.Error()
+}
+type ErrGetOwner struct { ErrOwnership }
+type ErrOwner struct { ErrOwnership }
+type ErrPerm struct { ErrOwnership }
+
 func PrivOwnership(file string) error {
 	// Check ownership and permissions of the private file:
 	// * must belong to the current user
@@ -19,28 +29,39 @@ func PrivOwnership(file string) error {
 	}
 
 	// Get system dependend struct stat, see man stat(2)
-	stat, ok := fi.Sys().(*syscall.Stat_t)
-	if !ok {
-		return fmt.Errorf(
-			"fails to retrieve the UID of the owner of %q from %T structure, possible the platform is not supported",
-			file, fi)
+	stat, err := sysStat(fi)
+	if err != nil {
+		return err
 	}
 
 	// Check ownership
 	if uint32(uid) != stat.Uid {
-		return fmt.Errorf(
+		return &ErrOwner{ErrOwnership{fmt.Errorf(
 			"UID of the user running the application is %d, but the UID of the owner of the file %q is %d - " +
 			"refusing to use this file because of a security breach, the file must belong to the application user",
-			uid, file, stat.Uid)
+			uid, file, stat.Uid)}}
 	}
 
 	// Check the file access mode
 	if mode := fi.Mode().Perm(); mode & 0o066 != 0 {
-		return fmt.Errorf("file %q must NOT be read/write accessible by the group/all users, " +
+		return &ErrPerm{ErrOwnership{fmt.Errorf(
+			"file %q must NOT be read/write accessible by the group/all users, " +
 			"only the application user must have read access to it, current permission mode is: %o",
-			file, mode)
+			file, mode)}}
 	}
 
 	// OK
 	return nil
+}
+
+// Get system dependend struct stat, see man stat(2)
+var sysStat = func(fi os.FileInfo) (*syscall.Stat_t, error) {
+	stat, ok := fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		return nil, &ErrGetOwner{ErrOwnership{fmt.Errorf(
+				"fails to retrieve the UID of the owner of %q from %T structure," +
+				" possible the platform is not supported", fi.Name(), fi)}}
+	}
+
+	return stat, nil
 }
---
+++ common/fschecks/priv_file_test.go
@@ -0,0 +1,191 @@
+//go:build linux
+package fschecks
+
+import (
+	"fmt"
+	"testing"
+	"os"
+	"io/fs"
+	"time"
+	"syscall"
+	"errors"
+	"path/filepath"
+)
+
+var tests = []struct{
+	ownerOk	bool
+	modeOk	bool
+	wantErr	error
+} {
+	{	// Correct owner and access mode 0600
+		ownerOk:	true,
+		modeOk:		true,
+	},
+	{	// Incorrect owner/mode
+		wantErr:	&ErrOwner{ErrOwnership{errors.New("incorrect owner")}},
+	},
+	{	// Incorrect mode
+		ownerOk:	true,
+		wantErr:	&ErrPerm{ErrOwnership{errors.New("incorrect access mode")}},
+	},
+}
+
+func TestPrivOwnership(t *testing.T) {
+	for testN, test := range tests {
+		// Get the name of test file
+		testFile, needRm, err := getTestFile(test.ownerOk, test.modeOk)
+		if err != nil {
+			t.Errorf("%v", err)
+			// Go to the next test
+			continue
+		} else if needRm {
+			// Cleanup temporary file on exit
+			defer os.Remove(testFile)
+		}
+
+		// Run function
+		if err = PrivOwnership(testFile); err == nil {
+			// Check that no errors wanted
+			if test.wantErr == nil {
+				// Ok, go to the next test
+				continue
+			}
+			//
+			t.Errorf("[%d] case successed, but must not, want error %T (%v)", testN, test.wantErr, test.wantErr)
+		} else if fmt.Sprintf("%T", err) == fmt.Sprintf("%T", test.wantErr) {
+			// Success, expected error
+		} else {
+			t.Errorf("unexpected error: got - %T (%v), want - %T (%v)", err, err, test.wantErr, test.wantErr)
+		}
+	}
+}
+
+func TestPrivOwnershipStatFail(t *testing.T) {
+	// Create a test file with random name
+	testFile, needRm, err := getTestFile(true, true)
+	if err != nil {
+		t.Errorf("cannot create temporary file: %v", err)
+	}
+	if needRm {
+		defer os.Remove(testFile)
+	}
+
+	// Create non-existing filename
+	nxFile := filepath.Join(testFile, "this-file-does-not-exist")
+
+	// Call PrivOwnership
+	switch err := PrivOwnership(nxFile); err.(type) {
+		case nil:
+			t.Errorf("case successed, but must not - returned success for non-existing file %q", nxFile)
+		case *fs.PathError:
+			// Success, expected error
+		default:
+			t.Errorf("case returned unexpected error type %T (%v), want *fs.PathError", err, err)
+	}
+}
+
+func TestErrOwnership(t *testing.T) {
+	const testEO = "test ErrOwnership error"
+	err := &ErrOwnership{errors.New(testEO)}
+	if err.Error() != testEO {
+		t.Errorf("ErrOwnership.String() returned %q, want - %q", testEO, testEO)
+	}
+}
+
+//
+// system stat() call testing
+//
+type fakeFI struct{}
+func (ffi *fakeFI) Name() string { return "fake-file" }
+func (ffi *fakeFI) Size() int64 { return 0 }
+func (ffi *fakeFI) Mode() fs.FileMode { return 0 }
+func (ffi *fakeFI) ModTime() time.Time { return time.Time{} }
+func (ffi *fakeFI) IsDir() bool { return false }
+func (ffi *fakeFI) Sys() any { return nil }
+
+func Test_sysStat(t *testing.T) {
+	switch _, err := sysStat(&fakeFI{}); err.(type) {
+	case *ErrGetOwner:
+		// Success, expected error
+	case nil:
+		t.Errorf("no errors on fake interface")
+		t.FailNow()
+	default:
+		t.Errorf("want retrieving ownership error, got - %T (%v)", err, err)
+	}
+}
+
+func Test_sysStat_fail(t *testing.T) {
+	testFile, needRm, err := getTestFile(false, false)
+	if err != nil {
+		t.Errorf("%v", err)
+		t.FailNow()
+	} else if needRm {
+		// Cleanup temporary file on exit
+		defer os.Remove(testFile)
+	}
+
+	// Replace sysStat by function that always retunrs error
+	sysStat = func(fi os.FileInfo) (*syscall.Stat_t, error) {
+		return nil, &ErrGetOwner{ErrOwnership{fmt.Errorf("negative case testing")}}
+	}
+
+	// Now, test ownership of this file
+	switch err := PrivOwnership(testFile); err.(type) {
+		case nil:
+			t.Errorf("PrivOwnership does not catch invalid file ownership")
+		case *ErrGetOwner:
+			// Success, expected error
+		default:
+			t.Errorf("want the incorrect ownership error, got - %T (%v)", err, err)
+	}
+}
+
+//
+// Auxiliary functions
+//
+
+func getTestFile(ownerOk, modeOk bool) (string, bool, error) {
+	// Need to get current effective user ID
+	uid := os.Geteuid()
+	if uid < 0 {
+		return "", false, fmt.Errorf("cannot get UID of current process - unsupported value %d", uid)
+	}
+
+	// If requested incorrect owner and I'm not root
+	if uid != 0 && !ownerOk {
+		// Return file that always has root:root & 0644 perms
+		return "/etc/passwd", false, nil
+	}
+
+	// Need to create temporary file
+	f, err := os.CreateTemp("", ".go-test-rche.")
+	if err != nil {
+		return "", false, fmt.Errorf("cannot create temporary file: %w", err)
+	}
+	// Close immediately, we do not needed it open
+	if err := f.Close(); err != nil {
+		return "", false, fmt.Errorf("cannot close temporary file: %w", err)
+	}
+
+	if uid == 0 && !ownerOk {
+		// Change the owner/group to "random" values
+		if err := os.Chown(f.Name(), 1825, 1825); err != nil {
+			return "", false, fmt.Errorf("cannot cange owner of created temporary file: %w", err)
+		}
+	}
+
+	if modeOk {
+		// Set correct access mode - only owner can read/write
+		if err := os.Chmod(f.Name(), 0o600); err != nil {
+			return "", false, fmt.Errorf("cannot set correct mode to the test file %q: %w", f.Name(), err)
+		}
+	} else {
+		// Set incorrect access mode - all can read
+		if err := os.Chmod(f.Name(), 0o644); err != nil {
+			return "", false, fmt.Errorf("cannot set incorrect mode to the test file %q: %w", f.Name(), err)
+		}
+	}
+
+	return f.Name(), true, nil
+}
