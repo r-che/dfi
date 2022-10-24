f2af6fd27210f0fcca28b404f991f881002ff335
RomanChebotarev <RomanCh@artel.im>
2022-10-24 20:09:34 +0300

    dbi/mongo - implement UpdateObj

--- dbi/mongo/agent.go
+++ dbi/mongo/agent.go
@@ -5,9 +5,17 @@ import (
 
 	"github.com/r-che/dfi/types"
 	"github.com/r-che/dfi/types/dbms"
-//	"github.com/r-che/dfi/dbi/common"
+	"github.com/r-che/dfi/dbi/common"
 
-//	"github.com/r-che/log"
+	"github.com/r-che/log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	MongoIDField	=	"_" + dbms.FieldID
+	ObjsCollection	=	"objs"
 )
 
 //
@@ -15,23 +23,90 @@ import (
 //
 
 func (mc *MongoClient) UpdateObj(fso *types.FSObject) error {
-	return fmt.Errorf("Not implemented")	// TODO
+	// Push object to update queue
+
+	if mc.ReadOnly {
+		log.W("(MongoCli:UpdateObj) R/O mode IS SET, Insert/Update of %q collection" +
+				" will NOT be performed => %s\n", ObjsCollection, mc.CliHost + ":" + fso.FPath)
+		// Increase the update counter and return no errors
+		mc.updated++
+		// OK
+		return nil
+	}
+	log.D("(MongoCli:UpdateObj) Insert/Update of collection %q => %s\n", ObjsCollection, mc.CliHost + ":" + fso.FPath)
+
+	// Get collection handler
+	coll := mc.c.Database(mc.Cfg.ID).Collection(ObjsCollection)
+
+	// Update/Insert object
+	id := common.MakeID(mc.CliHost, fso)
+	doc := bson.D{{`$set`, bson.D{
+		{MongoIDField,			id},
+		{dbms.FieldHost,		mc.CliHost},
+		{dbms.FieldName,		fso.Name},
+		{dbms.FieldFPath,		fso.FPath},
+		{dbms.FieldRPath,		fso.RPath},
+		{dbms.FieldType,		fso.Type},
+		{dbms.FieldSize,		fso.Size},
+		{dbms.FieldMTime,		fso.MTime},
+		{dbms.FieldChecksum,	fso.Checksum},
+	}}}
+
+	res, err := coll.UpdateOne(mc.Ctx,
+		bson.D{{MongoIDField, id}},			// Update exactly this ID
+		doc,
+		options.Update().SetUpsert(true),	// do insert if no object with this ID was found
+	)
+	if err != nil {
+		return fmt.Errorf("(MongoCli:UpdateObj) updateOne (id: %s) on %s.%s failed: %w",
+				id, coll.Database().Name(), coll.Name(), err)
+	}
+
+	if res.MatchedCount == 0 && res.UpsertedCount == 0 {
+		return fmt.Errorf("(MongoCli:UpdateObj) updateOne (id: %s) on %s.%s returned success," +
+			" but no documents were changed", id, coll.Database().Name(), coll.Name())
+	}
+
+	// Increase the update counter and return no errors
+	mc.updated++
+
+	// OK
+	return nil
+}
+
+func (mc *MongoClient) Commit() (int64, int64, error) {
+	// Reset state on return
+	defer func() {
+		// Reset counters
+		mc.updated = 0
+		mc.deleted = 0
+		// Reset lists of queued data
+		mc.toDelete = nil
+	}()
+
+	// Check for keys to delete
+	if nDel := len(mc.toDelete); nDel != 0 {
+		log.D("(MongoCli:Commit) Need to delete %d keys", nDel)
+
+		if mc.ReadOnly {
+			panic("(MongoCli:Commit) ReadOnly is not supported")	// TODO
+		} else {
+			// TODO
+		}
+
+
+		log.D("(MongoCli:Commit) Done deletion operation")
+	}
+
+	// XXX Use intermediate variables to avoid resetting return values by deferred function
+	ru, rd := mc.updated, mc.deleted
+
+	return ru, rd, nil
+}
+
+func (mc *MongoClient) LoadHostPaths(match dbms.MatchStrFunc) ([]string, error) {
+	return nil, fmt.Errorf("LoadHostPaths - Not implemented")	// TODO
 }
 func (mc *MongoClient) DeleteObj(fso *types.FSObject) error {
-	return fmt.Errorf("Not implemented")	// TODO
-}
-func (mc *MongoClient) Commit() (int64, int64, error) {
-	return -1, -1, fmt.Errorf("Not implemented")	// TODO
-}
-func (mc *MongoClient) SetReadOnly(ro bool) {
-	// TODO
-}
-func (mc *MongoClient) TermLong() {
-	// TODO
-}
-func (mc *MongoClient) Stop() {
-	// TODO
-}
-func (mc *MongoClient) LoadHostPaths(match dbms.MatchStrFunc) ([]string, error) {
-	return nil, fmt.Errorf("Not implemented")	// TODO
+	return fmt.Errorf("DeleteObj - Not implemented")	// TODO
 }
--- dbi/mongo/mongo.go
+++ dbi/mongo/mongo.go
@@ -2,18 +2,97 @@ package mongo
 
 import (
 	"fmt"
+	"reflect"
+	"strings"
 
 //	"github.com/r-che/dfi/types"
 	"github.com/r-che/dfi/types/dbms"
 //	"github.com/r-che/dfi/dbi/common"
 
 //	"github.com/r-che/log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoClient struct {
 	*dbms.CommonClient
+
+	c	*mongo.Client
+
+	// Dynamic members
+	toDelete	[]string
+	updated		int64
+	deleted		int64
 }
 
 func NewClient(dbCfg *dbms.DBConfig) (*MongoClient, error) {
-	return nil, fmt.Errorf("mongo.NewClient() not implemented")
+	// Initialize Mongo client
+	mc := &MongoClient{
+		CommonClient: dbms.NewCommonClient(dbCfg, dbCfg.CliHost),
+	}
+
+
+	// Check for credentials options
+	creds, err := parsePrivCfg(dbCfg.PrivCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create client options to connect
+	opts := options.Client().
+		ApplyURI(dbCfg.HostPort).
+		SetAuth(*creds)
+
+	if mc.c, err = mongo.Connect(mc.Ctx, opts); err != nil {
+		return nil, fmt.Errorf("(MongoCli:NewClient) cannot create new client to %s: %w", dbCfg.HostPort, err)
+	}
+
+	return mc, nil
+}
+
+func parsePrivCfg(pcf map[string]any) (creds *options.Credential, err error) {
+	// Check for empty configuration
+	if pcf == nil {
+		// OK, just return nothing
+		return nil, nil
+	}
+
+	// Setting configuration parameter using reflect.Set may raise panic, need to handle it
+	var parseField string
+	defer func() {
+		if p := recover(); p != nil {
+			// Clear read configuration
+			creds = nil
+			// Check for panic value has "string" type
+			if s, ok := p.(string); ok {
+				// Try to remove unnecessary "reflect.Set:" prefix from the panic value
+				p = strings.TrimPrefix(s, "reflect.Set: ")
+			}
+			// Set error
+			err = fmt.Errorf("(MongoCli:parsePrivCfg)" +
+				" cannot parse private configuration field %q: %v", parseField, p)
+		}
+	}()
+
+	// Fill options.Credential structure using reflection, list of available fields see there:
+	// https://pkg.go.dev/go.mongodb.org/mongo-driver/mongo/options#Credential
+	creds = &options.Credential{}
+
+	s := reflect.ValueOf(creds).Elem()
+	for i := 0; i < s.NumField(); i++ {
+		// Get the field name
+		parseField = s.Type().Field(i).Name
+		// Get the field value from configuration
+		v, ok := pcf[parseField]
+		if !ok {
+		// Skip if the field does not exists in the pcf
+			continue
+		}
+
+		// Set the field value
+		s.Field(i).Set(reflect.ValueOf(v))
+	}
+
+	return creds, nil
 }
