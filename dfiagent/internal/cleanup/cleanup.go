b1808ec45819b117abe250e0bd1e1b284c66c039
RomanChebotarev <RomanCh@artel.im>
2022-11-16 19:32:32 +0300

    golangci-lint:dfiagent/internal/cleanup - simplify Run (cyclop)

--- dfiagent/internal/cleanup/cleanup.go
+++ dfiagent/internal/cleanup/cleanup.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/r-che/log"
 	"github.com/r-che/dfi/types"
+	"github.com/r-che/dfi/types/dbms"
 	"github.com/r-che/dfi/dbi"
 	"github.com/r-che/dfi/dfiagent/internal/cfg"
 )
@@ -22,6 +23,7 @@ func Run() error {
 	if err != nil {
 		return err
 	}
+
 	// Check if read-only mode is required
 	if c.DBReadOnly {
 		dbc.SetReadOnly(true)
@@ -29,13 +31,38 @@ func Run() error {
 
 	log.I("(Cleanup) Started")
 
+	// Load paths belong to the current host
+	toDel, err := loadHostPaths(dbc, c.IdxPaths)
+	if err != nil {
+		return fmt.Errorf("(Cleanup) cannot load host paths: %w", err)
+	}
+
+	// Delete outdated paths
+	for _, path := range toDel {
+		if err := dbc.DeleteObj(&types.FSObject{FPath: path}); err != nil {
+			log.E("(Cleanup) Cannot delete object for path %q: %v", path, err)
+		}
+	}
+
+	// Commit deletion
+	if _, deleted, err := dbc.Commit(); err != nil {
+		log.E("(Cleanup) Fail to delete objects from DB: %v", err)
+	} else {
+		log.I("(Cleanup) Cleaned up %d objects from DB", deleted)
+	}
+
+	// OK
+	return nil
+}
+
+func loadHostPaths(dbc dbms.ClientController, configuredPaths []string) ([]string, error) {
 	// Counters of not configured record and stale (not existing on FS) records
 	nc, nx := 0, 0
 
 	// Make a function to match records that should be deleted from DB
 	match := func(path string) bool {
 		// Iterate through list of paths configured for indexing
-		for _, confPath := range c.IdxPaths {
+		for _, confPath := range configuredPaths {
 			if strings.HasPrefix(path, confPath) {
 				// OK, this path inside of configured directories
 				goto fsCheck
@@ -43,7 +70,7 @@ func Run() error {
 		}
 
 		// This path is outside of configured directories, append to delete
-		log.D("(Cleanup) Path %q does not belong to configured directories (%v)", path, c.IdxPaths)
+		log.D("(Cleanup) Path %q does not belong to configured directories (%v)", path, configuredPaths)
 		nc++
 
 		// Skip filesystem check because it does not make sense,
@@ -75,31 +102,16 @@ func Run() error {
     // Load from database all objects that belong to the current host
 	toDel, err := dbc.LoadHostPaths(match)
 	if err != nil {
-		return fmt.Errorf("(Cleanup) cannot load list of objects paths belong to this host: %w", err)
+		return nil, fmt.Errorf("DB client controller error: %w", err)
 	}
 
 	// Check for stale data
 	if nc + nx == 0 {
 		log.I("(Cleanup) Nothing to clean")
-		return nil
+		return nil, nil
 	}
 
 	log.I("(Cleanup) %d not configured and %d non-existing records found", nc, nx)
 
-	// Delete outdated paths
-	for _, path := range toDel {
-		if err := dbc.DeleteObj(&types.FSObject{FPath: path}); err != nil {
-			log.E("(Cleanup) Cannot delete object for path %q: %v", path, err)
-		}
-	}
-
-	// Commit deletion
-	if _, deleted, err := dbc.Commit(); err != nil {
-		log.E("(Cleanup) Fail to delete objects from DB: %v", err)
-	} else {
-		log.I("(Cleanup) Cleaned up %d objects from DB", deleted)
-	}
-
-	// OK
-	return nil
+	return toDel, nil
 }
